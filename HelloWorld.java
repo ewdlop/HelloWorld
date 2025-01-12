5f027e67e6d81c2b0cd13967adb5612cf45949cb
Raymond Lei(雷菩宇); 䨻-Ficus religiosa-Cousin <ray810815@gmail.com>
2025-01-12 03:24:33 -0500

    Create HelloWorld.java
    public class HelloWorld {
        public static void main(String[] args) {
            String name = "Alice";
            greet(name);
        }
    `
        public static void greet(String name) {
            System.out.println("Hello " + name);
        }
    }``

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Alice";
+        greet(name);
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+}
